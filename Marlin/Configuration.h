0c2a9b8ce3ae3695eb0eaa794ed8d4ac49329c93
kmobs <kmobs@kmobli.com>
2020-05-10 19:36:21 -0500

    Increase voltages to fix skipping. Offset Z again.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -972,7 +972,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { +48, -2, -0.125 }
+#define NOZZLE_TO_PROBE_OFFSET { +48, -2, -0.900 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
